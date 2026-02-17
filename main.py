236853b182434ab85e53a4e3892d17b0e5cc14ab
idlingturtle <jiipee@jupron.net>
2026-02-17 19:19:08 +0200

    Playarea set up

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import pygame
+from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
+    pygame.init()
     print("Starting Asteroids with pygame version:",pygame.version.ver)
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:",SCREEN_HEIGHT)
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+
+        for event in pygame.event.get():
+            pass
+        
+        pygame.Surface.fill(screen, (0,0,0))
+
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 if __name__ == "__main__":
     main()
