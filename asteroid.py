7f8c017add025b0a7bc63fdef93ec0335acfa5eb
idlingturtle <jiipee@jupron.net>
2026-02-25 10:47:55 +0200

    added splitting of the asteroids and scoring

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+from logger import log_event
 from circleshape import CircleShape
 from constants import *
 
@@ -12,3 +14,24 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += self.velocity * dt
+    
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+
+            angle = random.uniform(20, 50)
+
+            pos_vector = self.velocity.rotate(angle)
+            neg_vector = self.velocity.rotate(-angle)
+
+            NEW_RADIUS = self.radius - ASTEROID_MIN_RADIUS
+
+            asteroid1 = Asteroid(self.position.x, self.position.y, NEW_RADIUS)
+            asteroid2 = Asteroid(self.position.x, self.position.y, NEW_RADIUS)
+
+            asteroid1.velocity = self.velocity + pos_vector * 1.2
+            asteroid2.velocity = self.velocity + neg_vector * 1.2
--- main.py
+++ main.py
@@ -12,6 +12,12 @@ def main():
 
     print("Starting Asteroids!")
     
+    # Title to the game windows
+    pygame.display.set_caption("Asteroids!")
+
+    # Set up font for score on screen
+    fontObj = pygame.font.Font(None, 40)
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
 
@@ -29,6 +35,7 @@ def main():
     asteroidField = AsteroidField()
 
     dt: int = 0
+    score: int = 0
 
     while True:
 
@@ -40,6 +47,14 @@ def main():
         
         screen.fill("black")
 
+        # set up the score screen and keep it updated
+        score_screen = fontObj.render(f"Score: {score}", True, "white", None)
+        textRectObj = score_screen.get_rect()
+        textRectObj.topleft = (0,0)
+    
+        # write the score to the screen
+        screen.blit(score_screen, textRectObj)
+
         updatable.update(dt)
 
         for sprite in drawable:
@@ -49,12 +64,20 @@ def main():
             if asteroid.collides_with(player):
                 log_event("player_hit")
                 print("GAME OVER! TRY AGAIN.")
+                print(f"Your score was {score}")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
+                    score += 1
 
         pygame.display.flip()
 
         # limit FPS to 60
         dt = clock.tick(60)/1000
+        
         
 
 if __name__ == "__main__":
